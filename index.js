8c603cb72056d6ea96f35741ad6e75dafe25b469
madeleinvanniekerk <madelein@offerzen.com>
2022-06-27 11:34:59 +0200

    update action

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const core = require('@actions/core');
 try {
   const path = core.getInput('path');
   console.log(`Hello ${path}!`);
+  console.log(`AWS_ACCESS_KEY_ID: ${process.env.AWS_ACCESS_KEY_ID}`)
 }
 catch (error) {
   core.setFailed(error.message);
