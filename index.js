034859a0122b1f29a866ffd52775350d8443f549
madeleinvanniekerk <madelein@offerzen.com>
2022-06-27 11:26:08 +0200

    update action

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const core = require('@actions/core');
 
 try {
-  // `who-to-greet` input defined in action metadata file
   const ssmParamName = core.getInput('ssm_param_name');
   console.log(`Hello ${ssmParamName}!`);
   const value = "someValueFromAwsSDK";
