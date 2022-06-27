f597cda98f65931441d923d436387c1fd55cdc6e
madeleinvanniekerk <madelein@offerzen.com>
2022-06-27 11:53:39 +0200

    update action

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const core = require('@actions/core');
 const AWS = require('aws-sdk');
 
-try {
-  const path = core.getInput('path');
-  console.log(`Hello ${path}!`);
+async function run_action() {
+  try {
+    // const path = core.getInput('path');
+    const path = '/'
+    console.log(`Hello ${path}!`);
 
-  const ssm = new AWS.SSM();
-  const params =
-    {
-      Path: path,
-      Recursive: true,
-      WithDecryption: true
-    };
+    AWS.config.update({region: process.env.AWS_DEFAULT_REGION});
+    const sts = new AWS.STS();
+    sts.getCallerIdentity({}, function (err, data) {
+      if (err) console.log(err, err.stack);
+      else console.log(data);
+    });
 
-  const result = await ssm.getParametersByPath(params).promise();
-  console.log(`result: ${JSON.stringify(result)}`)
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
-catch (error) {
-  core.setFailed(error.message);
-}
+
+run_action();
